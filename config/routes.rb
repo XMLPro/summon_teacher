39bf16c22456a0f543aecd7468b63d4984537c99
cut-ter <c0114436cc@edu.teu.ac.jp>
2017-04-17 18:13:25 +0900

    chat実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get  'chat', to: 'chat#index'
+  mount ActionCable.server => '/cable'
 end
