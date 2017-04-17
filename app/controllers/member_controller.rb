25a507984e1063e19160bf30614428e373e8a663
cut-ter <c0114436cc@edu.teu.ac.jp>
2017-04-17 22:18:42 +0900

    なかったときに作るように修正

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -8,7 +8,7 @@ class MemberController < ApplicationController
       render
     elsif request.post?   
       # 現状複数クラス想定してないからとりあえず固定
-      class_room = ClassRoom.find_by_name('KE302')
+      class_room = ClassRoom.find_or_create_by(name: 'KE302')
       seat = class_room.seats.find_by_num(params['seat']['num'])
       if seat.blank?
         @error = '席が存在しません'
