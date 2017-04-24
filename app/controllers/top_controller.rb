197e341258d886024f9ad83f414610f7d612fe64
CsTarepanda <sankaku900622@yahoo.co.jp>
2017-04-24 16:19:36 +0900

    use position

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,9 @@
 class TopController < ApplicationController
   def index
+    seat_id = session['seat_id']
+    if seat_id
+      seat = Seat.find_by_id(seat_id)
+      @seat = seat
+    end
   end
 end
