40c53980ed07e21ea6da6d159b0074eb68060502
CsTarepanda <sankaku900622@yahoo.co.jp>
2017-04-24 16:06:57 +0900

    add summon button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'member#index'
+  root to: 'top#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get   '/register', to: 'member#register'
